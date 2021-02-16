ad1103e9cfb82e0ce04dc04ced96f9d9a4604c97
wiktorstacel <stawik964@gmail.com>
2021-02-16 16:44:44 +0100

    Forms: express server to receive data after submitting

---
+++ server/server.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', function(req, res){
+  res.send('Hello from server');
+});
+
+app.post('/enroll', function(req, res){
+  console.log(req.body);
+  res.status(200).send({"message": "Data received"});
+});
+
+app.listen(PORT, function(){
+  console.log("Server running on localhost: "+PORT);
+});
--- src/app/code1/code1.component.ts
+++ src/app/code1/code1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from '../enrollment.service';
 
 @Component({
   selector: 'app-code1',
@@ -10,10 +11,11 @@ export class Code1Component implements OnInit {
   
   topics = ['Angular', 'React', 'Vue'];
   topicHasError = true;
+  submitted = false;
   
   userModel = new User('', 'rob@test.com', 453453453, 'default', 'morning', true);
 
-  constructor() { }
+  constructor(private _EnrollmentService: EnrollmentService) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +26,15 @@ export class Code1Component implements OnInit {
     } else {
       this.topicHasError = false;
     }
+  }
+  
+  onSubmit() {
+    //console.log(this.userModel);
+    this.submitted = true;
+    this._EnrollmentService.enroll(this.userModel).subscribe(
+        data => console.log('Success!', data),
+        error => console.log('Error', error)
+      )
   }
 
 }
